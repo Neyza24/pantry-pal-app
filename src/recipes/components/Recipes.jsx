024c89d841d0fbbf3e0f55b6cf2e88b04a0d1719
Neyza <93302540+Neyza24@users.noreply.github.com>
2024-04-30 08:47:45 -0500

    feat: Get recipes filter by ingredients from food api (#4)

--- src/recipes/components/Recipes.jsx
+++ src/recipes/components/Recipes.jsx
@@ -1,19 +1,12 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
-import { fetchRecipes } from "../../store/recipes/thunks";
+
+import { useSelector } from "react-redux"
 import { RecipeCard } from "./RecipeCard";
 import { Box } from "@mui/material";
 
 
 export const Recipes = () => {
 
-    const dispatch = useDispatch();
     const { recipes } = useSelector(state => state.recipes);
-
-
-    useEffect(() => {
-        dispatch(fetchRecipes()); //fetchRecipes('https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2)
-    }, [dispatch])
 
     return (
         <Box sx={{ display: 'flex', flexWrap: 'wrap', marginLeft: 'auto', marginRight: 'auto', gap: '20px' }}>
