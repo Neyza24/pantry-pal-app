a39b3def56c5d43c96664c86f7f54c6c728b6ad3
Neyza <93302540+Neyza24@users.noreply.github.com>
2024-05-14 10:24:54 -0500

    feat: implement recipe detail route (#12)

---
+++ src/recipes/pages/RecipeDetailPage.jsx
@@ -0,0 +1,18 @@
+// import { useParams } from "react-router-dom"
+import { AppLayout } from "../layout/AppLayout"
+
+
+export const RecipeDetailPage = () => {
+
+
+    // const {id} = useParams();
+
+    return (
+        <AppLayout>
+            <div>Recipe Detail Page</div>
+        </AppLayout>
+        
+    )
+}
+
+/* eslint react/prop-types: 0 */
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { IngredientsPage } from '../recipes/pages/IngredientsPage'
 import { RecipesPage } from '../recipes/pages/RecipesPage'
+import { RecipeDetailPage } from '../recipes/pages/RecipeDetailPage'
 
 export const AppRouter = () => {
     return (
         <Routes>
             <Route path='/' element={<IngredientsPage />} />
             <Route path='/recipes' element={<RecipesPage />} />
+            <Route path='/recipes/:id' element={<RecipeDetailPage />} />
             <Route path='/*' element={<Navigate to='/' />} />
         </Routes>
     )
