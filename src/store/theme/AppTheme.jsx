71dcd9f50bd28d52ba4693eb217516b8fb65f5c2
Neyza <93302540+Neyza24@users.noreply.github.com>
2024-05-30 11:26:54 -0500

    implement theme mode state (#27)

--- src/store/theme/AppTheme.jsx
+++ src/store/theme/AppTheme.jsx
@@ -1,21 +1,18 @@
+import { useSelector } from 'react-redux';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { getAppTheme} from './getAppTheme';
-import { useState } from 'react';
+import { getAppTheme } from './getAppTheme';
 
 
 
 export const AppTheme = ({ children }) => {
+    const {mode} = useSelector(state => state.theme);
 
-    const [mode] = useState('light');
     const appTheme = createTheme(getAppTheme(mode));
-
-    
 
     return (
         <ThemeProvider theme={appTheme}>
             <CssBaseline />
-
             {children}
         </ThemeProvider>
     )
